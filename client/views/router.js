88657fbecedae4e44dee1c20d7a3d1df963ee9ed
gvsi <me@giovannialcantara.com>
2015-05-29 19:33:52 +0200

    Revert "reset to root to blankPage"
    This reverts commit 1cec6e5d1f1bfc6c69b99d328bc0d8e13619c15c.

--- client/views/router.js
+++ client/views/router.js
@@ -2,7 +2,7 @@
 Router.route('/', function () {
   //ADD YOU ROUTES HERE
   //eg : this.render('home');
-  this.render('blankPage');
+  this.render('social');
 },{
  layoutTemplate:"defaultLayout" 
 });
