b4f208c83de9095455e494e288c00c1c0b5e56dd
Martin Georgiev <martingeorgiev1995@gmail.com>
2015-08-08 22:42:16 +0100

    TinyMCE - design, code plugin, maths plugin

--- client/views/post/postCompose.js
+++ client/views/post/postCompose.js
@@ -3,123 +3,28 @@ Template.postCompose.rendered = function(){
     $("#postList").ioslist();
   }
 
-  mathquill();
-  var LatexImages = false;
-  $(function(){
-    function printTree(html)
-    {
-      html = html.match(/<[a-z]+|<\/[a-z]+>|./ig);
-      if(!html) return '';
-      var indent = '\n', tree = '';
-      while(html.length)
-      {
-        var token = html.shift();
-        if(token.charAt(0) === '<')
-        {
-          if(token.charAt(1) === '/')
-          {
-            indent = indent.slice(0,-2);
-            if(html[0] && html[0].slice(0,2) === '</')
-            token += indent.slice(0,-2);
-          }
-          else
-          {
-            tree += indent;
-            indent += '  ';
-          }
-          token = token.toLowerCase();
-        }
+  console.log("render");
 
-        tree += token;
-      }
-      return tree.slice(1);
-    }
-    var editingSource = false, latexSource = $('#latex-source'), htmlSource = $('#html-source'), codecogs = $('#codecogs'), latexMath = $('.mathquill-editor').bind('keydown keypress', function()
-    {
-      setTimeout(function() {
-        htmlSource.text(printTree(latexMath.mathquill('html')));
-        var latex = latexMath.mathquill('latex');
-        if(!editingSource)
-        latexSource.val(latex);
-        if(!LatexImages)
-        return;
-        latex = encodeURIComponent(latexSource.val());
-        //            location.hash = '#'+latex; //extremely performance-crippling in Chrome for some reason
-        codecogs.attr('src','http://latex.codecogs.com/gif.latex?'+latex).parent().attr('href','http://latex.codecogs.com/gif.latex?'+latex);
-      });
-    }).keydown().focus();
+//  tinymce.PluginManager.load('equationeditor', '/tinymce/plugins/equationeditor/plugin.min.js');
+  tinymce.EditorManager.execCommand('mceRemoveEditor',true, "tinymceTextArea");
+  $("#summernote-wrapper").append('<textarea id="tinymceTextArea" name="content"></textarea>');
+  tinymce.init({
+             selector: "#tinymceTextArea",
+             plugins: "link , image, sh4tinymce, equationeditor",
+             min_height: 400,
+             content_css: '/tinymce/plugins/equationeditor/mathquill.css',
+             menu: {},
+             menubar: false,
+             toolbar: "undo | redo | bold | italic | alignleft | aligncenter | alignright | alignjustify | link | unlink | image | sh4tinymce | equationeditor",
+             resize: false,
+             preview_styles: false,
+             statusbar: false,
+         });
 
-    // var tags = Courses.findOne(Router.current().params.course_id).tags;
-    //
-    //   if(tags){
-    //     tags.forEach(function(tag) {
-    //       $('#postTags').append('<label class="btn btn-complete"><input type="checkbox" checked="">'+ tag +'</label>');
-    //     });
-    //   }
 
-  });
 
-  $('#summernote-compose').summernote({
-    height: 400,
-    toolbar: [
-      ['misc', ['undo','redo','fullscreen']],
-      ['style', ['bold', 'italic', 'underline']],
-      ['insert', ['picture', 'link']],
-      ['para', ['ul', 'ol', 'paragraph']]
-    ],
-    onfocus: function(e) {
-      $('body').addClass('overlay-disabled');
-    },
-    onblur: function(e) {
-      $('body').removeClass('overlay-disabled');
-    },
-    onpaste: function(e) {
-      var bufferText = ((e.originalEvent || e).clipboardData || window.clipboardData).getData('Text');
-      e.preventDefault();
-      document.execCommand('insertText', false, bufferText);
-    },
-    oninit: function() {
-      // Add "open" - "save" buttons
 
-      $('#latexToolbarBtn').remove();
-      var noteBtn = '<button id="latexToolbarBtn" type="button" class="btn btn-default btn-sm btn-small" title="LaTeX Equation Editor" data-event="something" tabindex="-1"><span style="font-size:1.3em">&#931;</span></button>';
-
-      var fileGroup = '<div class="note-file btn-group">' + noteBtn + '</div>';
-
-      var sel;
-      var cursorPos;
-      var oldContent;
-
-      $(fileGroup).appendTo($('.note-toolbar'));
-      // Button tooltips
-      $('#latexToolbarBtn').tooltip({container: 'body', placement: 'bottom'});
-      // Button events
-      $('#latexToolbarBtn').click(function(event) {
-        sel = window.getSelection();
-        if (sel.anchorNode) {
-          cursorPos = sel.anchorOffset;
-          oldContent = sel.anchorNode.nodeValue;
-        }
-        $('#latexEditorModal').modal('show');
-      });
-      $('#insertLatexBtn').click(function(event) {
-        event.preventDefault();
-        $('#latexEditorModal').modal('hide');
-        $('#summernote-compose').summernote({focus:true});
-
-        setTimeout(function(){
-          var toInsert = "$"+$('#latex-source').val()+"$";
-          //console.log(cursorPos);
-          if (!oldContent) {
-            $('#summernote-compose').code("<p>"+toInsert+"</p>")
-          } else {
-            var newContent = oldContent.substring(0, cursorPos) + toInsert + oldContent.substring(cursorPos);
-            sel.anchorNode.nodeValue = newContent;
-          }
-        }, 500);
-      });
-    }
-  });
+   mathquill();
 
   if ($(window).width() < 1024) {
     $('.post-list').hide();
@@ -190,9 +95,11 @@ Template.postCompose.events({
        tags.push($( this ).text().trim());
     });
 
+    var tinymceText = tinyMCE.get('tinymceTextArea').getContent();
+
     var post = {
       title: $("#postTitleInput").val(),
-      text:  $('#summernote-compose').code(),
+      text: tinymceText,
       isAnonymous: $('#anonymous').is(':checked'),
       course_id: Router.current().params.course_id,
       tags: tags
