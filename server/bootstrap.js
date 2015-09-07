c7973577e2ec1f448fabd0a91e0085274b71ff60
Giovanni Alcantara <me@giovannialcantara.com>
2015-09-07 14:44:52 +0100

    Cleaning and refactoring

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -5,4 +5,5 @@ Meteor.startup(function() {
   process.env.MAIL_URL = 'smtp://postmaster%40sandboxa2fe94f745734744a0c72bb155d0f05a.mailgun.org:c898efff537a660081bdd08385beff4f@smtp.mailgun.org:587';
 
   SyncedCron.start();
+  Kadira.connect('64y4kmecnXbtEtDyB', 'f19c03c0-0bd7-4139-8fa0-8958fab4fcd9');
 });
