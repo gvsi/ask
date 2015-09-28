e4d5fd0d80504d6ceb158aab7d48656fd73e1d2a
Giovanni Alcantara <me@giovannialcantara.com>
2015-09-28 23:32:07 +0100

    Use global live URL

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -49,26 +49,6 @@ Template.home.helpers({
 });
 
 Template.home.events({
-  'submit #register-form' : function(e, t) {
-    e.preventDefault();
-    var username = t.find('#account-username').value;
-
-    // Trim and validate the input
-
-    Accounts.createUser({username: username, password: username}, function(err){
-      if (err) {
-        console.log('error');
-      } else {
-        console.log('success');
-      }
-
-    });
-
-    return false;
-  },
-  'click .courseFront' : function(e, t){
-    Router.go('room', {courseId: this._id});
-  },
   'submit #login-form' : function(e, t){
     e.preventDefault();
     // retrieve the input field values
