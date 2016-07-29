cd6b24414f3ffe4c2735a4f1f7b5df4e72fbf857
Giovanni Alcantara <me@giovannialcantara.com>
2016-07-29 17:39:55 +0100

    Finish healthcheck page

--- client/views/healthcheck/healthcheck.js
+++ client/views/healthcheck/healthcheck.js
@@ -1,4 +1,20 @@
 Template.healthcheck.helpers({
+    generalInfo: function() {
+        var environment;
+        if (Meteor.isDevelopment) {
+            environment = "Development"
+        } else if (Meteor.isProduction) {
+            environment = "Production"
+        } else if (Meteor.isTest) {
+            environment = "Test"
+        }
+        return {
+            title: "General information",
+            absoluteUrl: Meteor.absoluteUrl(),
+            environment: environment,
+            serverTime: new Date(TimeSync.serverTime())
+        }
+    },
     serverConnection: function(){
         var status = Meteor.status();
 
@@ -29,6 +45,51 @@ Template.healthcheck.helpers({
                 bg: 'bg-danger',
                 status: 'Offline',
                 description: 'User has disconnected the connection'
+            }
+        }
+    },
+    analytics: function(){
+        var settings = Meteor.settings.public;
+
+        if (settings.analyticsSettings && settings.analyticsSettings['Google Analytics'].trackingId == "UA-80980189-1") {
+            return {
+                title: 'Analytics',
+                bg: 'bg-success',
+                status: 'Tracking',
+                description: 'Google Analytics is active and tracking activity on the platform. Tracking is using the <b>development</b> tracking code.'
+            }
+        } else if (settings.analyticsSettings && settings.analyticsSettings['Google Analytics'].trackingId == "UA-51184647-6") {
+            return {
+                title: 'Analytics',
+                bg: 'bg-success',
+                status: 'Tracking',
+                description: 'Google Analytics is active and tracking activity on the platform. Tracking is using the <b>production</b> tracking code.'
+            }
+        } else {
+            return {
+                title: 'Analytics',
+                bg: 'bg-warning',
+                status: 'Not Tracking',
+                description: 'Google Analytics doesn\'t seem to be active. No activity is being tracked.'
+            }
+        }
+    },
+    databaseConnection: function() {
+        var status = Meteor.users.find({}).fetch();
+
+        if (status) {
+            return {
+                title: 'Database connection',
+                bg: 'bg-success',
+                status: 'Connected',
+                description: 'The database is connected.'
+            }
+        } else {
+            return {
+                title: 'Database connection',
+                bg: 'bg-danger',
+                status: 'Offline',
+                description: 'The database is not connected'
             }
         }
     }
